f351723b71756ef2cef4c4a8ed43ad234c7b0152
bitcargoCrew <sr_social@outlook.com>
2024-06-06 07:56:23 +0200

    adapt landing page according to new design

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -29,13 +29,13 @@ const Home: React.FC<HomeProps> = () => {
         sticky="top"
       >
         <Container>
-          <Navbar.Brand href="#home">MicroLYX</Navbar.Brand>
+          <Navbar.Brand href="#home">Quill</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Solution</Nav.Link>
-              <Nav.Link href="#link">Story</Nav.Link>
+              <Nav.Link href="#solution">Solution</Nav.Link>
+              <Nav.Link href="#story">Story</Nav.Link>
             </Nav>
             <div className="justify-content-end">
               {!buttonClicked && (
@@ -60,75 +60,82 @@ const Home: React.FC<HomeProps> = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div>
+      <div className={styles.backgroundContainer} id="home">
         <img
-          src="/pink_background_image.jpeg"
-          alt="Pink Background Image"
+          src="/background_image_creator.jpg"
+          alt="Creator Background Image"
           className={styles.backgroundImage}
         />
-        <h1 className={styles.backgroundText}>
-          The future of the internet: Microtransactions
-        </h1>
+        <p className={styles.backgroundText}>
+          “Web3 has struggled to drive consumer adoption. Quill enables the UX
+          of Web2 while offering the benefits of Web3. Quill gives creators
+          ownership and control of their content distribution/monetization with
+          a direct view to their end consumer"
+        </p>
+      </div>
+
+      <RootLayout>
+        <Row className={styles.centeredText} id="solution">
+          <h1>
+            Quill is a content platform that integrates micropayments for the
+            consumption of content (SocialFi). Why should you join?
+          </h1>
+        </Row>
+        <Row className={styles.rowSpace}>
+          <Col>
+            <h3>Income Generation for Creators</h3>
+            <p>
+              Creators need effective ways to monetize their content through
+              page traffic. We offer this by integrating the Lukso blockchain as
+              a micopayment solution.
+            </p>
+          </Col>
+          <Col>
+            <h3>Creators Meet Consumers</h3>
+            <p>
+              Creators have a platform to know who consumes their content. We
+              provide detailed statistics about your customers so that you can
+              tailor your content to your target group.
+            </p>
+          </Col>
+          <Col>
+            <h3>Low Transaction Fees</h3>
+            <p>
+              Traditional payment methods involve high transaction fees that
+              reduce the overall profit for creators. We avoid this by using the
+              Lukso blockchain as a payment layer.
+            </p>
+          </Col>
+          <Col>
+            <h3>Avoiding Hefty Subscriptions</h3>
+            <p>
+              Users are deterred by expensive monthly subscriptions for content
+              they find interesting but may not consume regularly. We offer a
+              flexible way to consume content with a pay-what-you-read
+              mechanism.
+            </p>
+          </Col>
+        </Row>
+      </RootLayout>
+      <div className={styles.backgroundContainer}>
+        <img
+          src="/quote_image.jpg"
+          alt="Creator Quote Image"
+          className={styles.backgroundImage}
+        />
+        <p className={styles.backgroundText}>
+          "Artists should own their own work for so many reasons, but the most
+          screamingly obvious one is that if you own what you create, you have
+          control over what happens to it." ​<p></p>
+          <p>Taylor Swift, World Renowned Performing Artist</p>
+        </p>
       </div>
       <RootLayout>
-        <Row className={styles.rowSpace}>
-          <Col>
-            <h1>Lukso Copyright Management</h1>
-            <p>
-              This Lukso demo app addresses the issue of intellectual property
-              rights rights and aims to ensure fair compensation for content
-              creators. The app makes it possible to pay a small fee, known as a
-              micropayment, to the creator for visiting their page.
-            </p>
-          </Col>
-          <Col>
-            {" "}
-            <img
-              src="/solution_image.jpeg"
-              alt="Solution Image"
-              className={styles.solutionImage}
-            />
-          </Col>
+        <Row className={styles.centeredText} id="story">
+          <h1>How can Quill achieve this?</h1>
         </Row>
-        <Row className={styles.rowSpace}>
-          <h1>Why should you care?</h1>
-          <Col>
-            <h3>Royalties and Compensation</h3>
-            <p>
-              Authors of a page receive royalties for the usage of their content
-              based on page visits. This compensation model aimes to ensure that
-              content creators are fairly rewarded for their contributions.
-            </p>
-          </Col>
-          <Col>
-            <h3>Micropayments and Transactions</h3>
-            <p>
-              I envision a system of micropayments to facilitate the transfer of
-              royalties between content creators and users. Whenever a user
-              accessed copyrighted content, a small payment will be made to the
-              author or rights holder. These micropayments will accumulate over
-              time, allowing authors to receive compensation for the ongoing
-              usage of their work.
-            </p>
-          </Col>
-          <Col>
-            <h3>Digital Identity</h3>
-            <p>
-              Every creator and user requires a digital identity (e.g. a
-              universal profile). This identity is responsible for displaying
-              the creator of the page and managing micropayments in the
-              background.
-            </p>
-          </Col>
-        </Row>
-        <Row className={styles.rowSpace}>
-          <h1>How does it work??</h1>
-          <Col>
-            <h3>You can find a demo video here:</h3>
-            <p>
-              Video
-            </p>
-          </Col>
+        <Row className={styles.centeredText}>
+          <p>Our explanation video will be published soon.</p>
         </Row>
       </RootLayout>
     </div>
